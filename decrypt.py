7dfa8e99707fb5c1c3ba821531fc6a42553c3210
nimba005 <nimbaoyugi@gmail.com>
2023-07-04 17:36:53 +0300

    Initial commit

--- decrypt.py
+++ decrypt.py
@@ -16,3 +16,20 @@ def lasso_letter(letter, shift_amount):
     # Send the decoded letter back
     return decoded_letter
 print(lasso_letter('a', 2))
+
+# Define a function to find the truth in a secret message
+# Shift the letters in a word by a specified amount to discover the hidden word
+def lasso_word(word, shift_amount):
+    # This variable is updated each time another letter is coded
+    decoded_word = ""
+    # This for loop iterates through each letter in the word parameter
+    for letter in word:
+        # The lasso_letter() function is invoked with each letter and the shift amount
+        # The result (the decoded letter) is sorted in a variable called decoded_letter
+        decoded_letter = lasso_letter(letter, shift_amount)
+        # The decoded_letter value is added to the end of the decoded_word value
+        decoded_word = decoded_word + decoded_letter
+    # The decoded_word is sent back to the line of code that invoked this function
+    return decoded_word
+# Try to decode the word "terra"
+print("Shift terra by 13 gives: \n" + lasso_word("terra", 13))
